3898de4a6bfa12f6cbfd11d86262a96d4bc432ea
vinodhnartu120940 <vinodhnartu120940@gmail.com>
2022-05-04 17:17:23 +0530

    second commit

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 let arrayofObject =
     [
         {
-            id: 28,
-            Title: "Sweden"
+            Name: "Sachin Tendulkar",
+            Image: "./images/sachin.jpg",
+            Player: "Indian",
+            Role: "AllRounder"
         },
         {
-            id: 56,
-            Title: "USA"
+            Name: "Ricky Ponting",
+            Image: "./images/ponting1.webp",
+            Player: "Australia",
+            Role: "Batsman"
         },
         {
-            id: 89,
-            Title: "England"
-        },
-        {
-            id: 89,
-            Title: "England"
-        },
+            Name: "Rohit Sharma",
+            Image: "./images/rohit.webp",
+            Player: "Indian",
+            Role: "Batsman"
 
+        },
         {
-            id: 89,
-            Title: "England"
+            Name: "Virat Kohli",
+            Image: "./images/kohli.jpg",
+            Player: "Indian",
+            Role: "Batsman"
+
         }
+        // {
+        //     id: 56,
+        //     Title: "USA"
+        // },
+        // {
+        //     id: 89,
+        //     Title: "England"
+        // },
+        // {
+        //     id: 89,
+        //     Title: "England"
+        // },
     ]
 let html = ``;
 //console.log(arrayofObject[0])
@@ -28,12 +45,13 @@ let html = ``;
 for (let i = 0; i < arrayofObject.length; i++) {
     let value = arrayofObject[i];
     html += `
-    <div class="card" id="view">
-    <div class="card-block" id="view">
+    <div class="card m-5" id="view">
+    <img class="card-img-top" src="${value.Image}" alt="Card image" style="width:100%">
+    <div class="card-body" id="view">
    
-    <h4 class="card-title">Employee</h4>
-    <h6 class="card-subtitle text-muted" id="id">Id:-${value.id}</h6>
-    <p class="card-text p-y-1" id="title">Title:-${value.Title}</p>
+    <h4 class="card-title">${value.Name}</h4>
+    <h6 class="card-subtitle text-muted" id="id">Player:  ${value.Player}</h6>
+    <p class="card-text p-y-1 " id="title">${value.Role}</p>
     <!-- <a href="#" class="card-link">link</a>
     <a href="#" class="card-link">Second link</a> -->
     </div>
